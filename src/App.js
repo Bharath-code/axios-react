8d400d640378881bc2e878014ebf6a798c53dd2d
Bharath-code <kumarbharath63@gmail.com>
2018-07-08 16:28:42 +0530

    Final axios demo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserForm from "./components/UserForm";
+import axios from "axios";
+
+class App extends Component {
+
+  state = {
+    repos : null
+  }
+  getUser = (e) => {
+      
+        e.preventDefault();
+        const user = e.target.elements.username.value;
+        if(user){
+          axios.get(`https://api.github.com/users/${user}`)
+          .then((res) => {
+            const repos = res.data.public_repos;
+            this.setState({
+              repos
+            });
+          })
+        }else return;
+
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome HTTP Axios</h1>
+        </header>
+        <UserForm getUser= {this.getUser}/>
+        {this.state.repos ? <p> Number of repos: {this.state.repos}</p> :
+          <p>Please enter a username</p>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
